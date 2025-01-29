1f0980b137afd6fc2d7a466f275d88bd5658439b
Quvonchbek <saodatovquvonch8@gmail.com>
2025-01-29 21:30:59 +0500

    Update Darslar.py

--- Darslar.py
+++ Darslar.py
@@ -1,1 +1,22 @@
-print("hammaga salom")
+class Catalog:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, title, author, year):
+        book = {
+            "title": title,
+            "author": author,
+            "year": year
+        }
+        self.books.append(book)
+
+    def display_books(self):
+        for book in self.books:
+            print(f"Title: {book['title']}, Author: {book['author']}, Year: {book['year']}")
+
+catalog = Catalog()
+catalog.add_book("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+catalog.add_book("To Kill a Mockingbird", "Harper Lee", 1960)
+catalog.add_book("1984", "George Orwell", 1949)
+
+catalog.display_books()
