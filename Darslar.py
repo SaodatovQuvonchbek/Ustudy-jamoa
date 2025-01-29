c85bf9e4b37d152a51d7e87d09c20e68968d6abc
Quvonchbek <saodatovquvonch8@gmail.com>
2025-01-29 21:33:45 +0500

    df
    drgd

--- Darslar.py
+++ Darslar.py
@@ -13,15 +13,18 @@ class Catalog:
     def display_books(self):
         for book in self.books:
             print(f"Title: {book['title']}, Author: {book['author']}, Year: {book['year']}")
-<<<<<<< HEAD
+
 
 catalog = Catalog()
-=======
+
 catalog = Catalog()
 
->>>>>>> b9b365f1c138de6689d36baa62625dba1ac9e035
-catalog.add_book("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+
+catalog.add_book("The 43534534 Gatsby", "F. Scott Fitzgerald", 1925)
 catalog.add_book("To Kill a Mockingbird", "Harper Lee", 1960)
 catalog.add_book("1984", "George Orwell", 1949)
 
 catalog.display_books()
+
+
+
